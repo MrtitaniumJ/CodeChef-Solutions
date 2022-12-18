03acdcf20f8d8074a6e39aaf9bffc8feab0e4e7e
Jatin Sharma <87287391+MrtitaniumJ@users.noreply.github.com>
2022-12-18 15:50:53 +0530

    Create The Two Dishes.java

---
+++ Basic Programming/The Two Dishes/The Two Dishes.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner ja = new Scanner(System.in);
+		int T = ja.nextInt();
+		while(T-->0){
+		    int N = ja.nextInt();
+		    int S = ja.nextInt();
+		    
+		    if(S<=N) System.out.println(S);
+		    else System.out.println((2*N)-S);
+		}
+	}
+}
